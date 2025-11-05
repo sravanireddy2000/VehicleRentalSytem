02135358e549dbbbb8388b2f29efb92ee1c60815
Upputuris16@gmail.com <Upputuris16@gmail.com>
2025-11-05 13:43:29 +0530

    add_mongodb

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+
+client = MongoClient("mongodb://localhost:27017/")
+rentalDatabase = client["rental_database"]
+
 
 app = Flask(__name__)
+
 
 
 @app.route('/')
