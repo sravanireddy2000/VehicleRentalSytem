321a352d2e078e78eef5c4605ce6f1cd3be75cfb
Upputuris16@gmail.com <Upputuris16@gmail.com>
2025-12-01 19:56:32 +0530

    header_section_vehicle

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 
 client = MongoClient("mongodb://localhost:27017/")
-rentalDatabase = client["rental_database"]
+rentalDatabase = client["rentalDatabase"]
 
 
 app = Flask(__name__)
@@ -14,6 +14,9 @@ app = Flask(__name__)
 def HomePage():
    return render_template('index.html')
    
+@app.route('/vehicles')
+def VehiclesPage():
+   return render_template('vehicles.html')   
 if __name__ == '__main__':
    app.run(debug=True)
 
