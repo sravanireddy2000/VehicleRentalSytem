d4edb2974778ea7afce1a6d11b9e172bd2c8835b
Upputuris16@gmail.com <Upputuris16@gmail.com>
2025-12-06 14:45:12 +0530

    addvehicles_js

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,3 +1,28 @@
 document.getElementById('addForm').addEventListener('submit', function(e) {
             e.preventDefault();
-});
+             var data = {
+                brand: document.getElementById('brand').value,
+                model: document.getElementById('model').value,
+                type: document.getElementById('type').value,
+                year: document.getElementById('year').value,
+                pricePerDay: document.getElementById('pricePerDay').value
+            };
+            
+            fetch('/admin/add', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(response => response.json())
+            .then(result => {
+                if(result.success) {
+                    document.getElementById('message').innerHTML = 'Vehicle added!';
+                    document.getElementById('addForm').reset();
+                    loadVehicles();
+                }
+            });
+        });
+        
+        
